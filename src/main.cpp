75ac773ac5a1d3ac746a5cc823dc5e4536db810c
thweasel <thweasel@gmail.com>
2022-10-30 18:16:53 +0000

    Z80 Data and Address connected

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,30 @@
 #include <SPI.h>
 
 
+#define PORT_DDR_OUTPUT 255
+#define PORT_DDR_INPUT 0
+
 #define SBUS_STC_out 49
 #define SBUS_OE_out 48
 #define ESP_HARDLOCK 5
 
+#define Z80DATA PORTA
+#define Z80DATA_DDR DDRA
+
+#define Z80ADDR_L PORTF
+#define Z80ADDR_L_DDR DDRF
+
+#define Z80ADDR_H PORTK
+#define Z80ADDR_H_DDR DDRK
+
+
 /*
+  PORT A -- Z80 DATA
+
   PORT B -- 0123 SPI
   
-  PORT F (low) & K (high) -- Z80 ADDRESS
+  PORT F -- Z80 ADDRESS LOW
+  PORT K -- Z80 ADDRESS HIGH
 
 
 
@@ -36,6 +52,13 @@ void setup()
   digitalWrite(SBUS_OE_out,HIGH);
   digitalWrite(ESP_HARDLOCK,HIGH);
 
+
+  //Z80 Ports
+  Z80DATA_DDR = PORT_DDR_OUTPUT;
+  Z80ADDR_L_DDR = PORT_DDR_OUTPUT;
+  Z80ADDR_H_DDR = PORT_DDR_OUTPUT;
+
+
 }
 
 void loop()
@@ -47,6 +70,9 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 
+  Z80DATA = 255;
+  Z80ADDR_L = 255;
+  Z80ADDR_H = 255;
 
   // Load the Shift Regs
   SPI.beginTransaction(mySpiSettings);
